f6de4def7d2f1b69b4dbd4d0589e369c7dea2a8e
Frank Tran <frank.tran@live.ca>
2020-10-13 18:30:38 -0400

    Added Comments

--- Main.py
+++ Main.py
@@ -43,6 +43,7 @@ counter = 0
 itemPath = '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]'
 print('Retrieving Product Details...')
 
+# Iterates through 5 Pages
 pagesToSearch = 5
 while pagesToSearch > 0:
     driver.get(driver.current_url + "&page=" + str(pagesToSearch))
@@ -56,6 +57,7 @@ while pagesToSearch > 0:
         prev_price = 0.0
         counter = 0
 
+        # Search for 70 products on that page
         while counter <= 70:
             print('Counter', counter)
             try:
@@ -92,6 +94,7 @@ print('Identifying Best Deal and Lowest Price...')
 
 firstRun = True
 
+# Check if Search Terms are in the Product Name
 for product in products:
     containSearchWord = True
     for word in search_terms:
@@ -102,14 +105,17 @@ for product in products:
             lowest_price = product.price
             cheapest_product = product
             firstRun = False
+        # Compare the prices of each product to identify cheapest item
         elif product.price < lowest_price:
             lowest_price = product.price
             cheapest_product = product
+        # Identify product with the biggest discount
         discount = product.prev_price - product.price
         if discount > biggest_discount:
             biggest_discount = discount
             best_deal_product = product
 
+# Compile and store matching results into a JSON file
 with open('products.json', 'w') as json_file:
     data = {}
     data["Products"] = []
@@ -117,11 +123,13 @@ with open('products.json', 'w') as json_file:
         data["Products"].append(prod.serialize())
     json.dump(data, json_file, sort_keys=True, indent=4)
 
+# Print out the Lowest Price, and the Best Deal
 print('Cheapest Product')
 print(json.dumps(cheapest_product.serialize(), indent=4, sort_keys=True))
 print('Biggest Deal on Product')
 print(json.dumps(best_deal_product.serialize(), indent=4, sort_keys=True))
 
+# Opens the webpage to the best discounted product
 options = Options()
 webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
